a4bf95848212696dd66a71d61782fa5d2690941c
Kuroneko-Rares <luigi.galvan@berkeley.edu>
2020-09-16 20:05:10 -0700

    Added a couple API calls

--- my-app/client/src/utils/API.js
+++ my-app/client/src/utils/API.js
@@ -1,20 +1,60 @@
 import axios from "axios";
 
 export default {
-  // Gets all books
-  getBooks: function() {
-    return axios.get("/api/books");
+  
+  getChart: function(ticker) {
+    return axios({
+      "method":"GET",
+      "url":"https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-charts",
+      "headers":{
+      "content-type":"application/octet-stream",
+      "x-rapidapi-host":"apidojo-yahoo-finance-v1.p.rapidapi.com",
+      "x-rapidapi-key":"c4e5d2b842msh6cffedf191a26dfp159c71jsn2ec40e87511b",
+      "useQueryString":true
+      },"params":{
+      "region":"US",
+      "comparisons":"%5EGDAXI%2C%5EFCHI",
+      "symbol":ticker,
+      "interval":"5m",
+      "range":"1d"
+      }
+      });
   },
-  // Gets the book with the given id
-  getBook: function(id) {
-    return axios.get("/api/books/" + id);
+ 
+  getCurrPrice: function(ticker) {
+    axios({
+      "method":"GET",
+      "url":"https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/v2/get-quotes",
+      "headers":{
+      "content-type":"application/octet-stream",
+      "x-rapidapi-host":"apidojo-yahoo-finance-v1.p.rapidapi.com",
+      "x-rapidapi-key":"c4e5d2b842msh6cffedf191a26dfp159c71jsn2ec40e87511b",
+      "useQueryString":true
+      },"params":{
+      "symbols":ticker,
+      "region":"US"
+      }
+      })
+    .then(res => {return res.quoteResponse.result[0].regularMarketPrice})
   },
-  // Deletes the book with the given id
-  deleteBook: function(id) {
-    return axios.delete("/api/books/" + id);
-  },
-  // Saves a book to the database
-  saveBook: function(bookData) {
-    return axios.post("/api/books", bookData);
+
+  getMovers: function(){
+
+    return axios({
+      "method":"GET",
+      "url":"https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/v2/get-movers",
+      "headers":{
+      "content-type":"application/octet-stream",
+      "x-rapidapi-host":"apidojo-yahoo-finance-v1.p.rapidapi.com",
+      "x-rapidapi-key":"c4e5d2b842msh6cffedf191a26dfp159c71jsn2ec40e87511b",
+      "useQueryString":true
+      },"params":{
+      "region":"US",
+      "start":"0",
+      "lang":"en-US",
+      "count":"6"
+      }
+      })
+
   }
 };
