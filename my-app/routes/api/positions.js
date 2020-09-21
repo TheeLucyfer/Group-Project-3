3eb2b0a7197d4f8a5f0a452459ec855b66a3cbb4
zhangkai <zkdtckk@gmail.com>
2020-09-20 22:07:58 -0700

    DB working

--- my-app/routes/api/positions.js
+++ my-app/routes/api/positions.js
@@ -1,7 +1,10 @@
 const router = require("express").Router();
 const positionsController = require("../../controllers/positionsController")
 
+// matched with /api/positions/:email
 router
     .route("/:email")
     .get(positionsController.findUserPos)
     .post(positionsController.create)
+
+module.exports = router;
